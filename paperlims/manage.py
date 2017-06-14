111b99754999ae9999c9d6b7cb078e24e41ba7e8
Scott C Lohr <scott.lohr@gmail.com>
2017-06-14 12:24:23 -0700

    switch to django configurations

--- paperlims/manage.py
+++ paperlims/manage.py
@@ -4,8 +4,12 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "paperlims.settings")
+    os.environ.setdefault('DJANGO_CONFIGURATION', 'Base')
+
+
     try:
-        from django.core.management import execute_from_command_line
+    	from configurations.management import execute_from_command_line
+        #from django.core.management import execute_from_command_line
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
